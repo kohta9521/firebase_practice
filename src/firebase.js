331eaca9724cf08afcee10d0c068b4f646a9e833
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-13 10:33:31 +0900

    add all files about firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxDW2UZYNnrxaL4sUIV26e6ykdsj0Hgk4",
+  authDomain: "fir-react-tutorial-ed9c5.firebaseapp.com",
+  projectId: "fir-react-tutorial-ed9c5",
+  storageBucket: "fir-react-tutorial-ed9c5.appspot.com",
+  messagingSenderId: "757228578996",
+  appId: "1:757228578996:web:24c2b7b41a964a79120fe1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
