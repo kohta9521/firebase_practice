14c1d431fba277e6b2c45f798e85a7a53952bc96
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-14 11:05:45 +0900

    add 4/14 first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // データベースからデータを取得する
+    const postData = collection(db, "posts");
+    console.log(postData);
+    getDocs(postData).then((snapShot) => {
+      console.log(snapShot.docs.map((doc) => doc));
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1></h1>
